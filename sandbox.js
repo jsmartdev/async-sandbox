f3a7b0dcec09ba8a905d16528ffcf129c61bc654
Jesse Smart <jsmartdev@protonmail.com>
2024-06-14 15:12:54 -0400

    HTTP Requests and Status Codes

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,13 @@
 // async javascript course by Net-Ninja
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () => {
+  if (request.readyState === 4 && request.status === 200) {
+    console.log(request.responseText);
+  } else if (request.readyState === 4) {
+    console.log('Failed to fetch data!');
+  }
+})
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/' );
+request.send();
