d17f0d468b1be182da0a3282463861a8c4660680
Jesse Smart <jsmartdev@protonmail.com>
2024-06-14 15:33:04 -0400

    Callback Functions

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,25 @@
 // async javascript course by Net-Ninja
-const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText);
-  } else if (request.readyState === 4) {
-    console.log('Failed to fetch data!');
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback('failed to fetch data!', undefined);
+    }
+  })
+
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos' );
+  request.send();
+}
+
+getTodos((err, data) => {
+  console.log('callback fired!');
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(data)
   }
-})
-
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/' );
-request.send();
+});
