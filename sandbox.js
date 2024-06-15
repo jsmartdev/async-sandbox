bc9545a866d636d56abdf5ff9bda1fd64193bd1e
Jesse Smart <jsmartdev@protonmail.com>
2024-06-15 15:58:53 -0400

    lesson 7: promises

--- sandbox.js
+++ sandbox.js
@@ -1,22 +1,28 @@
 // async javascript course by Net-Ninja
 
-const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest();
-
-  request.addEventListener('readystatechange', () => {
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback('failed to fetch data!', undefined);
-    }
+const getTodos = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data)
+      } else if (request.readyState === 4) {
+        reject('error getting resource')
+      }
+    })
+    request.open('GET', resource );
+    request.send();
   })
-
-  request.open('GET', resource );
-  request.send();
 }
 
-getTodos( 'todos/luigi.json', (err, data) => {
+getTodos('todos/mario.json').then(data => {
+  console.log('promise resolved:', data)
+}).catch (err => {
+  console.log('promise rejected!', err);
+})
+
+/*getTodos( 'todos/luigi.json', (err, data) => {
   console.log(data);
   getTodos('todos/mario.json', (err, data) => {
     console.log(data);
@@ -24,4 +30,20 @@ getTodos( 'todos/luigi.json', (err, data) => {
       console.log(data);
     })
   })
-});
+});*/
+
+//Promise example
+
+/*const getSomething = () => {
+  return new Promise((resolve, reject) => {
+    //fetch something
+    //resolve('some data');
+    reject('some error')
+  });
+}
+
+getSomething().then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err)
+})*/
