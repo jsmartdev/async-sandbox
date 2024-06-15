7da133d8c4e52e3cdcbd217b5422815759af10ac
Jesse Smart <jsmartdev@protonmail.com>
2024-06-15 16:47:01 -0400

    lesson 10: async & await

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,6 @@
 // async javascript course by Net-Ninja
 
-const getTodos = (resource) => {
+/*const getTodos = (resource) => {
   return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
     request.addEventListener('readystatechange', () => {
@@ -26,30 +26,16 @@ getTodos('todos/mario.json').then(data => {
   console.log('promise 3 resolved:', data)
 }).catch (err => {
   console.log('promise rejected:', err)
-})
+})*/
 
-/*getTodos( 'todos/luigi.json', (err, data) => {
-  console.log(data);
-  getTodos('todos/mario.json', (err, data) => {
-    console.log(data);
-    getTodos('todos/yoshi.json', (err, data) => {
-      console.log(data);
-    })
-  })
-});*/
+//Fetch api and async await
 
-//Promise example
-
-/*const getSomething = () => {
-  return new Promise((resolve, reject) => {
-    //fetch something
-    //resolve('some data');
-    reject('some error')
-  });
+const getTodos = async () => {
+  const response = await fetch('todos/mario.json');
+  const data = await response.json();
+  return data;
 }
 
-getSomething().then(data => {
-  console.log(data);
-}).catch(err => {
-  console.log(err)
-})*/
+getTodos()
+  .then(data => console.log('resolved:', data))
+  .catch(err => console.log('rejected:', err));
