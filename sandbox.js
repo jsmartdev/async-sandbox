6a6b4ce2729ae59c157a37cb920e0a1b50822a06
Jesse Smart <jsmartdev@protonmail.com>
2024-06-24 14:53:22 -0400

    fixed typos

--- sandbox.js
+++ sandbox.js
@@ -32,10 +32,13 @@ getTodos('todos/mario.json').then(data => {
 
 const getTodos = async () => {
   const response = await fetch('todos/mario.json');
+  if (response.status !== 200) {
+    throw new Error('Cannot fetch data!')
+  }
   const data = await response.json();
   return data;
 }
 
 getTodos()
   .then(data => console.log('resolved:', data))
-  .catch(err => console.log('rejected:', err));
+  .catch(err => console.log('rejected:', err.message));
